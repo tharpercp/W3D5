96ae77f82197c6af7cf8f2897e177e03f483a8cf
Elliot <elliotandrewwilson@gmail.com>
2021-08-27 14:18:48 -0400

    add add_child and delete_child

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -15,6 +15,20 @@ class PolyTreeNode
         end
     end
 
+    def add_child(child_node)
+        if !@children.include?(child_node)
+            @children.push(child_node)
+            child_node.parent = self
+        end
+    end
+
+    def remove_child(child_node)
+        raise "argument is not a child of this node" unless @children.include?(child_node)
+        raise "node is not a child" if child_node.parent == nil
+        self.children.delete(child_node)
+        child_node.parent = nil
+    end
+
     
 
 
