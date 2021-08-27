a05d7918d546e0bf9999b28443f937ee5fe15c7d
tharpercp <tharpercp@gmail.com>
2021-08-27 10:41:00 -0700

    starting

---
+++ skeleton/lib/00_tree_node.rb
@@ -0,0 +1,28 @@
+class PolyTreeNode
+
+    def initialize(value)
+        @parent = nil
+        @children = []
+        @value = value
+    end
+
+
+
+
+
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    private
+    attr_reader :parent, :children, :value
+
+
+
+end
